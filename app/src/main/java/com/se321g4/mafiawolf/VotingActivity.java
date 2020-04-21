a20b79e9dd645a0cb072f3849e99c7963c5e7247
Jimmy <jaime97-eduardo@hotmail.com>
2020-04-21 18:03:58 -0400

    VotingActivity now contextually sets player icons and names for the voting screen.

--- app/src/main/java/com/se321g4/mafiawolf/VotingActivity.java
+++ app/src/main/java/com/se321g4/mafiawolf/VotingActivity.java
@@ -36,7 +36,10 @@ public class VotingActivity extends AppCompatActivity {
     private int lobbyPosition;
     private int lobbyCount;
     private int iconValue = 0;
-    private int[] playerIcons = new int[3];//stores the icon value for each player in the game other than the local clients player
+    private String playerName;
+    private int[] playerIcons;
+    private String[] playerNames;
+    private int helper;
 
 
     @Override
@@ -60,17 +63,35 @@ public class VotingActivity extends AppCompatActivity {
         players = FirebaseDatabase.getInstance().getReference().child("/Players");
         currentPlayer = FirebaseDatabase.getInstance().getReference().child("/Players").child("Player" + lobbyPosition);//allows the app to access the FireBase database*/
 
+        playerIcons = new int[lobbyCount-1];//stores the icon value for each player in the game other than the local clients player
+        playerNames = new String[lobbyCount-1]; //stores all player names for each player in the game other than the local clients player
+
         players.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(int i = 1; i <= lobbyCount; i++ ) {
-                    if(i == lobbyPosition){
+                for(int i = 0; i <= lobbyCount; i++) {
+                    if(i + 1 == lobbyPosition && lobbyPosition == 1){//this prevents a NullPointerException from occurring
                         continue;
                     }
+
+                    if(i + 1 == lobbyPosition || i == 0 || i == lobbyPosition){
+                        helper = i;
+                        i++;
+                    }
                     iconValue = dataSnapshot.child("Player" + i).child("icon").getValue(Integer.class);
-                    playerIcons[i-1] = iconValue;
+                    playerName = dataSnapshot.child("Player" + i).child("name").getValue().toString();
+
+                    if(helper > 0){
+                        playerIcons[helper-1] = iconValue;
+                        playerNames[helper-1] = playerName;
+                        helper++;
+                    }
+                    else{
+                        playerIcons[i-1] = iconValue;
+                        playerNames[i-1] = playerName;
+                    }
                 }
-                assignIcons();
+                assignIconsNames();//method assigns icons and names contextually for players on the voting screen
             }
 
             @Override
@@ -85,8 +106,9 @@ public class VotingActivity extends AppCompatActivity {
 
     }
 
-    private void assignIcons(){
+    private void assignIconsNames(){
         final int[] images = new int[7];//stores all references to the player icons in an array so that they can be accessed contextually
+
         images[1] = R.drawable.playericon1;
         images[2] = R.drawable.playericon2;
         images[3] = R.drawable.playericon3;
@@ -94,43 +116,62 @@ public class VotingActivity extends AppCompatActivity {
         images[5] = R.drawable.playericon5;
         images[6] = R.drawable.playericon6;
 
-//        for(int i = 1; i <= lobbyCount; i++ ){
-//            if(i == lobbyPosition){
-//                continue;
-//            }
-//            playerIcons.add(getIconInfo(i));
-//        }
-
-        for(int i = 1; i < 4; i++){//assigns icons contextually based on who's playing the game.
+        for(int i = 0; i <= lobbyCount; i++){//assigns icons contextually based on who's playing the game.
+            if(i +1 == lobbyPosition || i == 0 || i == lobbyPosition){ //this prevents a NullPointerException from occurring
+                helper = i;
+                i++;
+            }
             for(int j = 1; j < 7; j++ ){
-                if(i == 1 /*&& i != lobbyCount*/){
-                    if(j == playerIcons[0]){
-                        firstPlayer.setImageResource(images[j]);
-                        break;
+                if(helper > 0){//triggers helpers to maintain proper array iteration
+                    if(helper == 1){
+                        if(j == playerIcons[helper-1]){
+                            firstPlayer.setImageResource(images[j]);
+                            firstPlayerName.setText(playerNames[helper-1]);
+                            helper++;
+                            break;
+                        }
                     }
-                    continue;
-                }
-                else if(i == 2 /*&& i != lobbyCount*/){
-                    if(j == playerIcons[1]){
-                        secondPlayer.setImageResource(images[j]);
-                        break;
+                    else if(helper == 2){
+                        if(j == playerIcons[helper-1]){
+                            secondPlayer.setImageResource(images[j]);
+                            secondPlayerName.setText(playerNames[helper-1]);
+                            helper++;
+                            break;
+                        }
                     }
-                    continue;
-                }
-                else if(i == 3 /*&& i != lobbyCount*/){
-                    if(j == playerIcons[2]){
-                        thirdPlayer.setImageResource(images[j]);
-                        break;
+                    else if(helper == 3){
+                        if(j == playerIcons[helper-1]){
+                            thirdPlayer.setImageResource(images[j]);
+                            thirdPlayerName.setText(playerNames[helper-1]);
+                            helper++;
+                            break;
+                        }
                     }
-                    continue;
                 }
+                else {
+                    if (i == 1) {
+                        if (j == playerIcons[i - 1]) {
+                            firstPlayer.setImageResource(images[j]);
+                            firstPlayerName.setText(playerNames[i - 1]);
+                            break;
+                        }
+                    } else if (i == 2) {
+                        if (j == playerIcons[i - 1]) {
+                            secondPlayer.setImageResource(images[j]);
+                            secondPlayerName.setText(playerNames[i - 1]);
+                            break;
+                        }
+                    } else if (i == 3) {
+                        if (j == playerIcons[i - 1]) {
+                            thirdPlayer.setImageResource(images[j]);
+                            thirdPlayerName.setText(playerNames[i - 1]);
+                            break;
+                        }
+                    }
+                }
+
             }
         }//for
     }//assignIcons
-
-    private int getIconInfo(int i){
-        final int index = i;
-        return iconValue;
-    }//gets icon information from players in the database
 
 }
